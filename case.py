e8e4138655d4780ceec6eaea2007b142cc1aee8f
DorothéeCatry <114245234+DoroChan@users.noreply.github.com>
2024-12-17 14:46:32 +0100

    Create case.py

---
+++ case.py
@@ -0,0 +1,6 @@
+class Case:
+    """Classe représentant une case du plateau."""
+    def __init__(self, couleur, categorie, camembert):
+        self.couleur = couleur
+        self.categorie = categorie
+        self.camembert = camembert
